0cb84990db6911806c7b8320f98ada5b9791c8c6
Jim Shaw <jimxshaw@gmail.com>
2017-09-21 17:20:29 -0500

    Add styling to Card component.

--- src/card/Card.js
+++ src/card/Card.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 const Card = (props) => {
   return (
-    <div>
-      <img src='http://placehold.it/75' alt='insert img here' />
-      <div>Name here...</div>
-      <div>Company Name here...</div>
+    <div style={{margin: '1em'}}>
+      <img src='http://placehold.it/75' alt='insert img here'/>
+      <div style={{display: 'inline-block', marginLeft: 10}}>
+        <div style={{fontSize: '1.25em', fontWeight: 'bold'}}>
+          Name here...
+        </div>
+        <div>Company Name here...</div>
+      </div>
     </div>
   );
 };
