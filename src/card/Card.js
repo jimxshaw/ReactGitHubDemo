b24830bbbf15af000a0817daa1fa67d31467fedf
Jim Shaw <jimxshaw@gmail.com>
2017-09-21 17:13:20 -0500

    Add Card component.

---
+++ src/card/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = (props) => {
+  return (
+    <div>
+      <img src='http://placehold.it/75' alt='insert img here' />
+      <div>Name here...</div>
+      <div>Company Name here...</div>
+    </div>
+  );
+};
+
+export default Card;
