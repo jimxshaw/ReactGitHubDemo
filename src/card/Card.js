a7fe4b5ce14f935a22feaaa45381338462b6bc84
Jim Shaw <jimxshaw@gmail.com>
2017-09-21 17:25:56 -0500

    Add CardList component.

--- src/card/Card.js
+++ src/card/Card.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Card = (props) => {
   return (
     <div style={{margin: '1em'}}>
-      <img src='http://placehold.it/75' alt='insert img here'/>
+      <img width='75' src='https://avatars0.githubusercontent.com/u/5104397?v=4' alt='profile pic'/>
       <div style={{display: 'inline-block', marginLeft: 10}}>
         <div style={{fontSize: '1.25em', fontWeight: 'bold'}}>
-          Name here...
+          Jim Shaw
         </div>
-        <div>Company Name here...</div>
+        <div>Software Engineer</div>
       </div>
     </div>
   );
